23b883b4bfcc42d410049edf3e236bb95261b07d
Hakkar <zugrutus@gmail.com>
2022-12-20 20:31:14 +0000

    test1

--- src/library.js
+++ src/library.js
@@ -11,4 +11,18 @@ const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 const refs = {
     input: document.querySelector('.search-input'),
     list: document.querySelector('.film-list'),
+    watched: document.querySelector('.watched-btn'),
+    queue: document.querySelector('.queue-btn'),
 }
+
+
+const onWatch = () => {
+    console.log("this is test")
+};
+
+const onQueue = () => {};
+
+refs.watched.addEventListener('click', onWatch);
+refs.queue.addEventListener('click', onQueue);
+
+
