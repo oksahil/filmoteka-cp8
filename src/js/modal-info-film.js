d9ef4500afab0bffbbbc234c54e968a5e4dd9097
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-12-21 22:33:16 +0200

    Update modal-info-film.js
    add and remove evenlistener on Esc and Outside-click

--- src/js/modal-info-film.js
+++ src/js/modal-info-film.js
@@ -4,9 +4,10 @@ import { cardTemplate } from "/src/js/card-templete";
 
 console.log(refs);
 refs.list.addEventListener('click', onCardClick);
+// window.addEventListener()
 
 function onCardClick(e) {
-    console.dir(e.target.closest('.film-item').id);
+    // console.dir(e.target.closest('.film-item').id);
     const filmTemplateId =e.target.closest('.film-item').id;
     toggleModal();
 
@@ -18,6 +19,9 @@ function respFilmInfo(resp) {
     console.log(resp.data.popularity);
 
     refs.cardList.innerHTML = cardTemplate(resp.data);
+
+    window.addEventListener('keydown', onEscCloseModal);
+    refs.modal.addEventListener('click', onOutsideClickCloseModal);
 };
 
 //   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -25,5 +29,25 @@ refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+};
+
+function onEscCloseModal(e) {
+    if (e.code !== 'Escape') {
+        return;
+    }
+
+    toggleModal();
+    removeAddEventlisteners();
+};
+function onOutsideClickCloseModal(e) {
+    // console.log(e.currentTarget);
+    // console.log(e.target);
+    if (e.currentTarget !== e.target) return;
+    toggleModal();
+    removeAddEventlisteners();
+};
+function removeAddEventlisteners() {
+    window.removeEventListener('keydown', onEscCloseModal);
+    refs.modal.removeEventListener('click', onOutsideClickCloseModal);
 };
  
