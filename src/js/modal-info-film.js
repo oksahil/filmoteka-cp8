43083f6ba8708f76d1144c3538e9d631aea686d5
Serhii Markov <108286639+KomaNatoR@users.noreply.github.com>
2022-12-21 19:20:43 +0200

    sss

--- src/js/modal-info-film.js
+++ src/js/modal-info-film.js
@@ -1,14 +1,29 @@
-// import { refs } from '/src/index.js';
+import { refs } from '/src/index.js';
+import { fetchFilmInfo } from "/src/js/fetch-film-info";
+import { cardTemplate } from "/src/js/card-templete";
 
+console.log(refs);
+refs.list.addEventListener('click', onCardClick);
 
-// (() => {
-// console.log(refs.openModalBtn);
+function onCardClick(e) {
+    console.dir(e.target.closest('.film-item').id);
+    const filmTemplateId =e.target.closest('.film-item').id;
+    toggleModal();
+
+    fetchFilmInfo(filmTemplateId).then(respFilmInfo);
+};
+
+function respFilmInfo(resp) {
+    console.log(resp.data);
+    console.log(resp.data.popularity);
+
+    refs.cardList.innerHTML = cardTemplate(resp.data);
+};
 
 //   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle('is-hidden');
-//   }
+function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+};
  
-// })();
