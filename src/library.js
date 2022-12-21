dd2bbf805822bcb4e489d68f3c2b995e16d9ebbc
Hakkar <zugrutus@gmail.com>
2022-12-21 16:21:15 +0000

    api unnecesary

--- src/library.js
+++ src/library.js
@@ -5,51 +5,38 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getDatabase, ref, set } from 'firebase/database';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { firebaseConfig } from './firebase-config';
-import fireStorage from './fireStorage';
-
-
-
-
+import { firebaseConfig } from './js/Firebase/firebase-config';
+import fireStorage from './js/Firebase/fireStorage';
+// import { ApiService } from './js/Firebase/api-service';
 
 const DEBOUNCE_DELAY = 300;
 const KEY = '27a3692489226a6f77b57cb0bdb9ce9a';
 const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
-
-
 const app = initializeApp(firebaseConfig);
 
-
-
 const refs = {
-    input: document.querySelector('.search-input'),
-    list: document.querySelector('.film-list'),
-    watched: document.querySelector('.watched-btn'),
-    queue: document.querySelector('.queue-btn'),
-}
+  input: document.querySelector('.search-input'),
+  list: document.querySelector('.film-list'),
+  watched: document.querySelector('.watched-btn'),
+  queue: document.querySelector('.queue-btn'),
+};
 
 const data = {
   watchedInfo: {},
-  queueInfo: {}
-}
+  queueInfo: {},
+};
 const { watchedInfo, queueInfo } = data;
 
 new fireStorage(watchedInfo, queueInfo);
 
-
 const onWatched = () => {
-    console.log("this is test")
+  console.log('this is test');
 };
 
 const onQueue = () => {
-    console.log('anotha test');
+  console.log('anotha test');
 };
-
-
-
 
 refs.watched.addEventListener('click', onWatched);
 refs.queue.addEventListener('click', onQueue);
-
-
