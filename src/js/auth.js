d647d833e2ece0e97b70a113ecdf2736dc3f7863
oksahil <oksana.khylyk@gmail.com>
2022-12-22 20:18:11 +0100

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,29 +1,29 @@
-import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  signInWithPopup,
-  GoogleAuthProvider,
-  signOut,
-  onAuthStateChanged,
-} from 'firebase/auth';
-import { Notify } from 'notiflix';
+// import { initializeApp } from 'firebase/app';
+// import {
+//   getAuth,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+//   signOut,
+//   onAuthStateChanged,
+// } from 'firebase/auth';
+// import { Notify } from 'notiflix';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyBPXP1YlZBJsT7lpMpPzltgNCRbOoRLm9E',
-  authDomain: 'filmoteka-ab559.firebaseapp.com',
-  projectId: 'filmoteka-ab559',
-  storageBucket: 'filmoteka-ab559.appspot.com',
-  messagingSenderId: '734966919204',
-  appId: '1:734966919204:web:6326cd2f16a81192eae8b1',
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBPXP1YlZBJsT7lpMpPzltgNCRbOoRLm9E',
+//   authDomain: 'filmoteka-ab559.firebaseapp.com',
+//   projectId: 'filmoteka-ab559',
+//   storageBucket: 'filmoteka-ab559.appspot.com',
+//   messagingSenderId: '734966919204',
+//   appId: '1:734966919204:web:6326cd2f16a81192eae8b1',
+// };
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const provider = new GoogleAuthProvider();
 
-const btnAuth = document.querySelector('.js-btn-auth');
-const btnLibrary = document.querySelector('.js-library');
-console.log(btnAuth);
-console.log(btnLibrary);
+// const btnAuth = document.querySelector('.js-btn-auth');
+// const btnLibrary = document.querySelector('.js-library');
+// console.log(btnAuth);
+// console.log(btnLibrary);
 
 // function checkLogSatus() {
 //   btnLibrary.style.display = 'none';
@@ -41,20 +41,20 @@ console.log(btnLibrary);
 //   btnAuth.addEventListener('click', authHandler);
 // }
 
-function authHandler(e) {
-  if (e.currentTarget.getAttribute('actions') === 'logged') {
-    signOut(auth);
-    btnAuth.setAttribute('actions', 'out');
-    btnAuth.textContent = 'Log in';
-    btnLibrary.style.display = 'none';
-  } else {
-    try {
-      signInWithPopup(auth, provider).then(res =>
-        Notify.success(`Hi, ${res.user.displayName}`)
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-checkLogSatus();
+// function authHandler(e) {
+//   if (e.currentTarget.getAttribute('actions') === 'logged') {
+//     signOut(auth);
+//     btnAuth.setAttribute('actions', 'out');
+//     btnAuth.textContent = 'Log in';
+//     btnLibrary.style.display = 'none';
+//   } else {
+//     try {
+//       signInWithPopup(auth, provider).then(res =>
+//         Notify.success(`Hi, ${res.user.displayName}`)
+//       );
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   }
+// }
+// checkLogSatus();
