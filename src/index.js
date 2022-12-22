0e673ecd17de9ee09ae56d675064089ffd2a0beb
Alex <ais_alex@ukr.net>
2022-12-22 21:51:43 +0200

    Repair debonce

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { fetchFilmInfo } from '/src/js/fetch-film-info';
 import { fetchFilm } from '/src/js/fetch-film';
 // import { filmTemplate } from '/src/js/film-templete';
 import { listg } from '/src/js/fetch-genres';
-
+import { createPagination, destroyPagination } from './js/pagination';
 // import './js/auth';
 
 var debounce = require('lodash.debounce');
@@ -15,42 +15,103 @@ export const KEY = '27a3692489226a6f77b57cb0bdb9ce9a';
 export const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
 export const refs = {
-    input: document.querySelector('.search-input'),
-    list: document.querySelector('.film-list'),
-     // openModalBtn: document.querySelector('.films'),
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    card: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    cardList: document.querySelector('.card-list'),
-    error: document.querySelector('.error'),
+  input: document.querySelector('.search-input'),
+  list: document.querySelector('.film-list'),
+  // openModalBtn: document.querySelector('.films'),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  card: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+  cardList: document.querySelector('.card-list'),
+  error: document.querySelector('.error'),
+  searchForm: document.querySelector('.search-form'),
 };
+document.addEventListener('DOMContentLoaded', main);
+// const fetchFilmsWithDebounce = debounce(renderSearchFilms, DEBOUNCE_DELAY);
+// refs.input.addEventListener('input', fetchFilmsWithDebounce);
 
+async function renderPopularFilms() {
+  const filmsData = await fetchFilmPopularity();
+  const paginationButtons = createPagination(
+    filmsData.total_pages,
+    5,
+    filmsData.page
+  );
+  paginationButtons.render(document.querySelector('.pagination-wrapper'));
+  paginationButtons.onChange(e => {
+    fetchFilmPopularity(e.target.value);
+  });
+}
 
-document.addEventListener('DOMContentLoaded', fetchFilmPopularity());
-refs.input.addEventListener('input', debounce(fetchFilm), DEBOUNCE_DELAY);
+function initInputListener() {
+  const { input } = refs;
+  let paginationButtons;
 
+  const renderFilmsOnInputChange = async e => {
+    e.preventDefault();
+    const inputText = e.target.value;
+    if (inputText !== '') {
+      const filmsData = await fetchFilm(inputText, 1);
+      if (filmsData.total_pages > 0) {
+        paginationButtons = createPagination(
+          filmsData.total_pages,
+          5,
+          filmsData.page
+        );
+        paginationButtons.render(document.querySelector('.pagination-wrapper'));
+        paginationButtons.onChange(e => {
+          fetchFilm(inputText, e.target.value);
+        });
+      } else {
+        destroyPagination();
+      }
+    }
+  };
+  const renderFilmsWithDebounce = debounce(
+    renderFilmsOnInputChange,
+    DEBOUNCE_DELAY
+  );
+  input.addEventListener('input', renderFilmsWithDebounce);
+}
+
+async function main() {
+  refs.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+  });
+
+  initInputListener();
+  renderPopularFilms();
+}
 export let items = [];
 export let strGenres = [];
 
-export const filmTemplate = ({ poster_path, original_name, release_date, genre_ids, first_air_date,id, original_title,vote_average }) => {
-    renderIds(genre_ids, listg);
-    console.log('strGenres', strGenres);
-    if (genre_ids === '') {
-        genre_ids = "no ganeres";
-    }
-    if (release_date === "") {
-        release_date = "no relase date";
-    }
-    if (release_date === undefined) {
-        release_date = first_air_date;
-    }
-    if (original_name === undefined) {
-        original_name = original_title;
-    }
-    let dataFilm = release_date.slice(0, 4);
-    if (poster_path === null) {
-        return `<li class="film-item list" id="${id}">
+export const filmTemplate = ({
+  poster_path,
+  original_name,
+  release_date,
+  genre_ids,
+  first_air_date,
+  id,
+  original_title,
+  vote_average,
+}) => {
+  renderIds(genre_ids, listg);
+  console.log('strGenres', strGenres);
+  if (genre_ids === '') {
+    genre_ids = 'no ganeres';
+  }
+  if (release_date === '') {
+    release_date = 'no relase date';
+  }
+  if (release_date === undefined) {
+    release_date = first_air_date;
+  }
+  if (original_name === undefined) {
+    original_name = original_title;
+  }
+  let dataFilm = release_date.slice(0, 4);
+  if (poster_path === null) {
+    return `<li class="film-item list" id="${id}">
     <div class="films">
     <div class="text-img">
     Sorry,<br>poster for film don't find <br>:(
@@ -59,17 +120,18 @@ export const filmTemplate = ({ poster_path, original_name, release_date, genre_i
     <h3 class="film-genre">${strGenres} | ${dataFilm} <span>${vote_average}</span></h3>
     </div>
     </li>`;
-    } else {
-        return `<li class="film-item list" id="${id}">
+  } else {
+    return `<li class="film-item list" id="${id}">
     <div class="films">
     <img class="film-img" src="https://image.tmdb.org/t/p/w500/${poster_path}">
     <h2 class="film-title">${original_name}</h2>
-    <h3 class="film-genre">${strGenres} | ${dataFilm} <span class="vote-highlighted">${vote_average.toFixed(1)}</span></h3>
+    <h3 class="film-genre">${strGenres} | ${dataFilm} <span class="vote-highlighted">${vote_average.toFixed(
+      1
+    )}</span></h3>
     </div>
     </li>`;
-            }
+  }
 };
-
 
 // const cardTemplate = ({ poster_path, original_title, release_date, genre_ids, first_air_date, vote_average, vote_count, popularity }) => {
 //     renderIds(genre_ids, listg);
