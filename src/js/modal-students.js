342dc7a67fea07059e0273c81dbe7ddb7bd9e442
Bellmere <therise00@gmail.com>
2022-12-22 08:33:48 +0200

    add css and snow

--- src/js/modal-students.js
+++ src/js/modal-students.js
@@ -1,8 +1,8 @@
 const modalStudent = document.querySelector('.modal-student-js');
 const modalStudentOpen = document.querySelector('.student-open-js');
 const modalStudentClose = document.querySelector('.student-close-js');
-
-console.log(modalStudentOpen);
+const modalAudio = document.getElementById('audio-shedruk');
+const cnv = document.getElementById('cnv');
 
 modalStudentOpen.addEventListener('click', onClickOpen);
 modalStudentClose.addEventListener('click', onClickClose);
@@ -10,15 +10,31 @@ modalStudentClose.addEventListener('click', onClickClose);
 function onClickOpen(e) {
     e.preventDefault();
     modalStudent.style.display = 'block';
+    if (modalStudent.style.display = 'block') {
+        playSound();
+    }
 }
 
 function onClickClose(e) {
     e.preventDefault();
     modalStudent.style.display = 'none';
+    if (modalStudent.style.display = 'none') {
+        stopSound();
+    }
 }
 
 window.onclick = function (e) {
-    if (e.target == modalStudent) {
+    if (e.target == cnv) {
         modalStudent.style.display = 'none';
+        stopSound();
     }
+}
+
+function playSound() {
+    modalAudio.currentTime = 0;
+    modalAudio.play();
+  }
+
+function stopSound() {
+    modalAudio.pause();
 }
