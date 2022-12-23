1a9b37810bc6d9401214ac096c76c709094952d6
oksahil <oksana.khylyk@gmail.com>
2022-12-23 15:32:32 +0100

    d

--- src/index.js
+++ src/index.js
@@ -109,7 +109,8 @@ export const filmTemplate = ({
   if (original_name === undefined) {
     original_name = original_title;
   }
-  let dataFilm = release_date.slice(0, 4);
+    let dataFilm = release_date.slice(0, 4);
+ 
   if (poster_path === null) {
     return `<li class="film-item list" id="${id}">
     <div class="films">
@@ -132,76 +133,6 @@ export const filmTemplate = ({
     </li>`;
   }
 };
-
-
-// const cardTemplate = ({ poster_path, original_title, release_date, genre_ids, first_air_date, vote_average, vote_count, popularity }) => {
-//     renderIds(genre_ids, listg);
-//     if (genre_ids === '') {
-//         genre_ids = "no ganeres";
-//     }
-//     if (release_date === "") {
-//         release_date = "no relase date";
-//     }
-//     if (release_date === undefined) {
-//         release_date = first_air_date;
-//     }
-//     let dataFilm = release_date.slice(0, 4);
-//     if (poster_path === null) {
-//         return `<li class="card-item list"  data-modal-open>
-//     <img class="film-img" src="https://pixabay.com/get/gb7db569fc3e8b14cffd882555f170a29f60967eb4b440951996ecf4dacfea8c4e24bfffa9b9a672bc4413d9a61fbeb317cbfc98195588fe198e02464199770bf_640.jpg">
-//     </li>
-//     <li>
-//     <h2 class="film-title">${original_title}</h2>
-//     <ul class="info-film">
-//     <li>
-//     <p class="text-discription">Vote/Votes</p>
-//     <p class="text-discription">Popularity</p>
-//     <p class="text-discription">Original Title</p>
-//     <p class="text-discription">Genre</p>
-//     </li>
-//     <li>
-//     <p class="text-discription">${vote_average}/${vote_count}</p>
-//     <p class="text-discription">${popularity}</p>
-//     <p class="text-discription">${original_title}</p>
-//     <p class="text-discription">${strGenres}</p>
-//     </li>
-//     </ul>
-//     <h3>About</h3>
-//     <p>${overview}</p>
-//         <ul class="btn-list list">
-//             <li class="btn-item"><button class="add-watched-btn btn" type="button"><span class="btn-text">ADD TO WATCHED</span></button></li>
-//             <li class="btn-item"><button class="add-queue-btn btn" type="button"><span class="btn-text">ADD TO QUEUE</span></button></li>
-//         </ul>
-//     </li>`;
-//     } else {
-//         return `<li class="card-item list"  data-modal-open>
-//     <img class="film-img" src="https://image.tmdb.org/t/p/w500/${poster_path}">
-//     </li>
-//     <li>
-//     <h2 class="film-title">${original_title}</h2>
-//     <ul class="info-film">
-//     <li>
-//     <p class="text-discription">Vote/Votes</p>
-//     <p class="text-discription">Popularity</p>
-//     <p class="text-discription">Original Title</p>
-//     <p class="text-discription">Genre</p>
-//     </li>
-//     <li>
-//     <p class="text-discription">${vote_average}/${vote_count}</p>
-//     <p class="text-discription">${popularity}</p>
-//     <p class="text-discription">${original_title}</p>
-//     <p class="text-discription">${strGenres}</p>
-//     </li>
-//     </ul>
-//     <h3>About</h3>
-//     <p>${overview}</p>
-//         <ul class="btn-list list">
-//             <li class="btn-item"><button class="add-watched-btn btn" type="button"><span class="btn-text">ADD TO WATCHED</span></button></li>
-//             <li class="btn-item"><button class="add-queue-btn btn" type="button"><span class="btn-text">ADD TO QUEUE</span></button></li>
-//         </ul>
-//     </li>`;
-//     }
-// }
 
 function renderIds(genre_ids, listg) {
   console.log('genre_ids', genre_ids);
@@ -230,7 +161,4 @@ export function renderInfo(itemsInfo) {
   refs.cardList.innerHTML = '';
   refs.cardList.insertAdjacentHTML('beforeend', filmListInfo.join(''));
 }
-// refs.openModalBtn.addEventListener('click', textInfo);
-// function textInfo() {
-//     fetchFilmInfo();
-// }
+
--- src/js/fetch-film.js
+++ src/js/fetch-film.js
@@ -6,31 +6,29 @@ import { refs } from '/src/index';
 import Notiflix from 'notiflix';
 import { render } from '/src/index';
 
-export async function fetchFilm(name = 0, pageNumber = 1) {
+export async function fetchFilm(e, name = 0, pageNumber = 1) {
   fetchGenres();
-
-  refs.error.textContent = '';
-  try {
-    const response = await axios.get(
-      `${URL}${KEY}&page=${pageNumber}&query=${name}`
-    );
-    let items = response.data.results;
-    console.log(items);
-    if (items === []) {
-      refs.error.textContent =
-        'Search result not successful. Enter the correct movie name.';
+  const value = e.target.value.trim();
+  if (value) {
+    refs.error.textContent = '';
+    try {
+      const response = await axios.get(
+        `${URL}${KEY}&page=${pageNumber}&query=${name}`
+      );
+      let items = response.data.results;
+      console.log(items);
+      if (items.lenght === 0) {
+        fetchFilmPopularity();
+        refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
+      }
+      render(items);
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+      fetchFilmPopularity();
+      refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
+      refs.list.innerHTML = '';
+      return Notiflix.Notify.failure('Search result not successful. Enter the correct movie name.');
     }
-    render(items);
-    return response.data;
-  } catch (error) {
-    console.log(error.message);
-
-    refs.error.textContent =
-      'Search result not successful. Enter the correct movie name.';
-    refs.list.innerHTML = '';
-    return Notiflix.Notify.failure(
-      'Search result not successful. Enter the correct movie name.'
-    );
   }
 }
-
