f92bd9e020a2d35c5f37cefffc1011708f2653d2
Hakkar <zugrutus@gmail.com>
2022-12-23 01:49:58 +0000

    localStorage1

---
+++ src/js/localStorage.js
@@ -0,0 +1,30 @@
+export const getLocalSt = (key) => {
+    try {
+        const gotten = localStorage.getItem(key);
+        return (JSON.parse(gotten) || undefined);
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
+export const setLocalSt = (key, value) => {
+  try {
+      const stringer = JSON.stringify(value);
+      localStorage.setItem(key, setten);
+    
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
+export const remLocalSt = (key) => {
+  try {
+    return localStorage.removeItem(key);
+  } catch (error) {
+    console.log('Error', error);
+  }
+};
+
+export {
+    getLocalSt,
+    setLocalSt, 
+    remLocalSt
+}
