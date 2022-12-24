83f631963be66f095192e7bb973ccb4848fe0f2d
Bellmere <therise00@gmail.com>
2022-12-24 21:15:02 +0200

    add animation

--- src/js/post-marquee.js
+++ src/js/post-marquee.js
@@ -1,31 +1,50 @@
-import { posts } from '../posts';
-import maqruee from 'vanilla-marquee';
+class LoopingElement {
+    constructor (element, currentTranslation, speed) {
+        this.element = element;
+        this.currentTranslation = currentTranslation;
+        this.speed = speed;
+        this.direction = true;
 
-const galleryEl = document.querySelector('.marquee__list');
-const newPosts = createElement(posts);
+        this.lerp = {
+            current: this.currentTranslation,
+            target: this.currentTranslation,
+            ease: 0.1,
+        };
 
+        this.render();
+    }
 
-galleryEl.insertAdjacentHTML('afterbegin', newPosts);
+    lerpFunc(current, target, ease) {
+        this.lerp.current = current * (1 - ease) + target * ease;
+    }
 
-function createElement(gallery) {
-    return gallery.map(({ link, trailer, description, ganre }) => {
-        return `
-            <li class="gallery__item">
-                <a class="gallery__link" 
-                href="${trailer}">
-                    <div class="marquee__photo">
-                        <img
-                        class="gallery__image"
-                        src="${link}"
-                        alt="${description}"
-                        />
-                    </div>
-                    <div class="marquee__content">
-                        <p class="marquee__title">${description}</p>
-                        <p class="marquee__ganre">${ganre}</p>
-                    </div>
-                </a>
-            </li>
-        `;
-    }).join(''); 
+    forward() {
+        this.lerp.target += this.speed;
+        if (this.lerp.target > 100) {
+            this.lerp.current -= 100 * 2;
+            this.lerp.target -= 100 * 2;
+        }
+    }
+
+    animate() {
+        this.forward();
+        this.lerpFunc(this.lerp.current, this.lerp.target, this.lerp.ease);
+
+        this.element.style.transform = `translateX(${this.lerp.current}%)`;
+    }
+
+    render() {
+        this.animate();
+        window.requestAnimationFrame(() => this.render());
+    }
 }
+
+let elements = document.querySelectorAll('.dynamic__gallery__wrapper');
+console.log(elements[0]);
+console.log(elements[1]);
+
+// elements.forEach(element => {
+//     new LoopingElement(element, 100, 0.2);
+// });
+let newLol = new LoopingElement(elements[0], 0, 0.2);
+let highLol = new LoopingElement(elements[1], -100, 0.2);
--- src/posts.js
+++ src/posts.js
@@ -1,30 +1,64 @@
+import postImg from './images/Post/Emancipation.jpg';
+import adam from './images/Post/Black Adam.jpg';
+import avatar from './images/Post/avatar-2.jpg';
+import train from './images/Post/Bullet Train.jpg';
 export const posts = [
-    {
-        link: './images/Poster/Emancipation.jpg',
+    {   
+        link: `${postImg}`,
         trailer: '',
         description: 'Emancipation',
         ganre: 'Drama',
     },
     {
-        link: '/images/Poster/Emancipation.jpg',
+        link: `${adam}`,
+        trailer: '',
+        description: 'Black Adam',
+        ganre: 'Superhero Action Film',
+    },
+    {
+        link: `${avatar}`,
+        trailer: '',
+        description: 'Avatar 2',
+        ganre: 'Science Fiction',
+    },
+    {
+        link: `${train}`,
+        trailer: '',
+        description: 'Bullet Train',
+        ganre: 'Action',
+    },
+    {
+        link: `${postImg}`,
         trailer: '',
         description: 'Emancipation',
         ganre: 'Drama',
     },
     {
-        link: '/images/Poster/Emancipation.jpg',
+        link: `${postImg}`,
         trailer: '',
         description: 'Emancipation',
         ganre: 'Drama',
     },
     {
-        link: '/images/Poster/Emancipation.jpg',
+        link: `${postImg}`,
         trailer: '',
         description: 'Emancipation',
         ganre: 'Drama',
     },
     {
-        link: '/images/Poster/Emancipation.jpg',
+        link: `${postImg}`,
+        trailer: '',
+        description: 'Emancipation',
+        ganre: 'Drama',
+    },
+    {
+        link: `${postImg}`,
+        trailer: '',
+        description: 'Emancipation',
+        ganre: 'Drama',
+    },
+    {
+        link: `${postImg}`,
         trailer: '',
         description: 'Emancipation',
         ganre: 'Drama',
