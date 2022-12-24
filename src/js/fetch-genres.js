28e91c80f8628a4e551e8673ecb908919ab9a019
oksahil <108234649+oksahil@users.noreply.github.com>
2022-12-24 22:23:56 +0100

    Revert "Update index.js"

--- src/js/fetch-genres.js
+++ src/js/fetch-genres.js
@@ -6,17 +6,17 @@ import Notiflix from 'notiflix';
 export let listg = [];
 
 export async function fetchGenres() {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=${KEY}&language=en-US`
-    );
-
-    listg = response.data.genres;
-    console.log(listg);
-    return listg;
-  } catch (error) {
-    console.log(error.message);
-    refs.list.innerHTML = '';
-    return Notiflix.Notify.failure('Oops, there is no film with that name.');
-  }
-}
+  
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${KEY}&language=en-US`)
+        
+       listg = response.data.genres;
+        console.log(listg);
+        return listg;
+        } catch (error) {
+            console.log(error.message);
+            refs.list.innerHTML = '';
+            return Notiflix.Notify.failure('Oops, there is no film with that name.');
+            };
+ };
+       
