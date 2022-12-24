8205054e2c9ff5035269de21a8ea37d71ce0246d
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2022-12-24 18:18:22 +0200

    fix search error

--- src/js/fetch-film.js
+++ src/js/fetch-film.js
@@ -5,6 +5,8 @@ import { fetchGenres } from '/src/js/fetch-genres';
 import { refs } from '/src/index';
 import Notiflix from 'notiflix';
 import { render } from '/src/index';
+import { fetchFilmPopularity } from '/src/js/fetch-film-popularity';
+
 
 export async function fetchFilm(name = 0, pageNumber = 1) {
   fetchGenres();
@@ -16,7 +18,7 @@ export async function fetchFilm(name = 0, pageNumber = 1) {
       );
       let items = response.data.results;
       console.log(items);
-      if (items === []) {
+      if (items.length === 0) {
         fetchFilmPopularity();
         refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
       }
@@ -27,6 +29,32 @@ export async function fetchFilm(name = 0, pageNumber = 1) {
       fetchFilmPopularity();
       refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
       refs.list.innerHTML = '';
-      return Notiflix.Notify.failure('Search result not successful. Enter the correct movie name.');
+      // return Notiflix.Notify.failure('Search result not successful. Enter the correct movie name.');
+      return;
     }
   }
+
+// export async function fetchFilm(name = 0, pageNumber = 1) {
+//   fetchGenres();
+
+//     refs.error.textContent = '';
+//     try {
+//       const response = await axios.get(`${URL}${KEY}&page=${pageNumber}&query=${name}`);
+//       console.log(value);
+//       console.log(response);
+//       let items = response.data.results;
+//       console.log(items);
+//       if (items.length === 0) {
+//         fetchFilmPopularity();
+//         refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
+//       }
+//       render(items);
+//       return response.data;
+//     } catch (error) {
+//       console.log(error.message);
+//       fetchFilmPopularity();
+//       refs.error.textContent = 'Search result not successful. Enter the correct movie name.';
+//       refs.list.innerHTML = '';
+//       return; 
+//     }
+//   }
