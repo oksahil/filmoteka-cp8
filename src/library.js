910ba9ee0ed9d84c0169fcec48149a95783148ed
Hakkar <zugrutus@gmail.com>
2022-12-25 20:43:24 +0000

    library again

--- src/library.js
+++ src/library.js
@@ -8,5 +8,37 @@ import { setLocalSt, getLocalSt, remLocalSt } from './js/localStorage';
 var debounce = require('lodash.debounce');
 
 
+const WATCHED_KEY = 'watched';
+const QUEUE_KEY = 'queue';
+
+const renderLibraryFilms = (arr, key) => {
+  const markup = arr.map(obj => filmTemplate(obj)).join('');
+
+  refs.list.insertAdjacentHTML('beforeend', markup);
+};
+
+if (getLocalSt(WATCHED_KEY)) {
+  refs.list.innerHTML = '';
+  const watchFilm = [...getLocalSt(WATCHED_KEY)];
+  renderLibraryFilms(watchFilm, WATCHED_KEY);
+}
+if (getLocalSt(QUEUE_KEY)) {
+  refs.list.innerHTML = '';
+  const queueFilm = [...getLocalSt(QUEUE_KEY)];
+  renderLibraryFilms(queueFilm, QUEUE_KEY);
+}
+
+const onWatched = () => {
+  refs.list.innerHTML = '';
+  const watchFilm = [...getLocalSt(WATCHED_KEY)];
+  renderLibraryFilms(watchFilm, WATCHED_KEY);
+};
+
+const onQueue = () => {
+  refs.list.innerHTML = '';
+  const queueFilm = [...getLocalSt(QUEUE_KEY)];
+  renderLibraryFilms(queueFilm, QUEUE_KEY);
+};
+
 refs.watched.addEventListener('click', onWatched);
 refs.queue.addEventListener('click', onQueue);
