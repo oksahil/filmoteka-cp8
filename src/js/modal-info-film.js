eb87651b0af049f14dacce888e2d39ee0f579b95
oksahil <108234649+oksahil@users.noreply.github.com>
2022-12-25 22:51:54 +0100

    Revert "binary again"

--- src/js/modal-info-film.js
+++ src/js/modal-info-film.js
@@ -29,122 +29,122 @@ function respFilmInfo(resp) {
   // refs.cardList.innerHTML = cardTemplate(resp.data,currentPictSrc);
 
   //-------------------------------------WATCHED-QUEUE----------------------------------------
-  // const WATCHED_KEY = 'watched';
-  // const QUEUE_KEY = 'queue';
-  // const { data } = resp;
-  // const addQueue = document.querySelector('.add-queue-btn');
-  // const addWatched = document.querySelector('.add-watched-btn');
+  const WATCHED_KEY = 'watched';
+  const QUEUE_KEY = 'queue';
+  const { data } = resp;
+  const addQueue = document.querySelector('.add-queue-btn');
+  const addWatched = document.querySelector('.add-watched-btn');
 
-  // let watchArr = [];
-  // const watchObj = data;
-  // let queueArr = [];
-  // const queueObj = data;
-  // let stringedWatchArr;
-  // let stringedQueueArr;
+  let watchArr = [];
+  const watchObj = data;
+  let queueArr = [];
+  const queueObj = data;
+  let stringedWatchArr;
+  let stringedQueueArr;
 
-  // if (!getLocalSt(WATCHED_KEY)) {
-  //   setLocalSt(WATCHED_KEY, []);
-  // }
-  // if (!getLocalSt(QUEUE_KEY)) {
-  //   setLocalSt(QUEUE_KEY, []);
-  // }
+  if (!getLocalSt(WATCHED_KEY)) {
+    setLocalSt(WATCHED_KEY, []);
+  }
+  if (!getLocalSt(QUEUE_KEY)) {
+    setLocalSt(QUEUE_KEY, []);
+  }
 
-  // if (getLocalSt(WATCHED_KEY).find(obj => obj.id === data.id)) {
-  //   addWatched.textContent = 'Remove from watched';
-  //   addWatched.classList.add('active');
-  // }
-  // if (getLocalSt(QUEUE_KEY).find(obj => obj.id === data.id)) {
-  //   addQueue.textContent = 'Remove from queue';
-  //   addQueue.classList.add('active');
-  // }
+  if (getLocalSt(WATCHED_KEY).find(obj => obj.id === data.id)) {
+    addWatched.textContent = 'Remove from watched';
+    addWatched.classList.add('active');
+  }
+  if (getLocalSt(QUEUE_KEY).find(obj => obj.id === data.id)) {
+    addQueue.textContent = 'Remove from queue';
+    addQueue.classList.add('active');
+  }
 
-  // async function textWatchDelay() {
-  //   setTimeout(() => {
-  //     addWatched.textContent = 'Remove from watched';
-  //     addWatched.disabled = false;
-  //   }, 1000);
-  // }
-  // async function textQueueDelay() {
-  //   setTimeout(() => {
-  //     addQueue.textContent = 'Remove from queue';
-  //     addQueue.disabled = false;
-  //   }, 1000);
-  // }
-  // async function textWatchDelayPre() {
-  //   setTimeout(() => {
-  //     addWatched.textContent = 'Add to watched';
-  //     addWatched.disabled = false;
-  //   }, 1000);
-  // }
-  // async function textQueueDelayPre() {
-  //   setTimeout(() => {
-  //     addQueue.textContent = 'Add to queue';
-  //     addQueue.disabled = false;
-  //   }, 1000);
-  // }
-  // const onWatchedModalBtn = e => {
-  //   console.log('hello!');
-  //   e.preventDefault();
+  async function textWatchDelay() {
+    setTimeout(() => {
+      addWatched.textContent = 'Remove from watched';
+      addWatched.disabled = false;
+    }, 1000);
+  }
+  async function textQueueDelay() {
+    setTimeout(() => {
+      addQueue.textContent = 'Remove from queue';
+      addQueue.disabled = false;
+    }, 1000);
+  }
+  async function textWatchDelayPre() {
+    setTimeout(() => {
+      addWatched.textContent = 'Add to watched';
+      addWatched.disabled = false;
+    }, 1000);
+  }
+  async function textQueueDelayPre() {
+    setTimeout(() => {
+      addQueue.textContent = 'Add to queue';
+      addQueue.disabled = false;
+    }, 1000);
+  }
+  const onWatchedModalBtn = e => {
+    console.log('hello!');
+    e.preventDefault();
 
-  //   if (addWatched.classList.contains('active')) {
-  //     console.log(data.id);
-  //     let index = watchArr.indexOf(data.id);
-  //     watchArr.splice(index, 1);
-  //     stringedWatchArr = JSON.stringify(watchArr);
-  //     localStorage.setItem(WATCHED_KEY, stringedWatchArr);
-  //     console.log(stringedWatchArr);
-  //     Notiflix.Notify.failure('Removed from watched');
-  //     addWatched.classList.remove('active');
-  //     addWatched.textContent = 'Removed from watched';
-  //     addWatched.disabled = true;
-  //     textWatchDelayPre();
-  //     return;
-  //   } else {
-  //     watchArr = [];
-  //     watchArr.push(...getLocalSt(WATCHED_KEY));
-  //     watchArr.push(watchObj);
-  //     stringedWatchArr = setLocalSt(WATCHED_KEY, watchArr);
+    if (addWatched.classList.contains('active')) {
+      console.log(data.id);
+      let index = watchArr.indexOf(data.id);
+      watchArr.splice(index, 1);
+      stringedWatchArr = JSON.stringify(watchArr);
+      localStorage.setItem(WATCHED_KEY, stringedWatchArr);
+      console.log(stringedWatchArr);
+      Notiflix.Notify.failure('Removed from watched');
+      addWatched.classList.remove('active');
+      addWatched.textContent = 'Removed from watched';
+      addWatched.disabled = true;
+      textWatchDelayPre();
+      return;
+    } else {
+      watchArr = [];
+      watchArr.push(...getLocalSt(WATCHED_KEY));
+      watchArr.push(watchObj);
+      stringedWatchArr = setLocalSt(WATCHED_KEY, watchArr);
 
-  //     console.log(watchArr);
+      console.log(watchArr);
 
-  //     addWatched.textContent = 'Added from watched';
-  //     addWatched.disabled = true;
-  //     textWatchDelay();
-  //     addWatched.classList.add('active');
-  //   }
-  // };
-  // const onQueueModalBtn = e => {
-  //   console.log('hello!');
-  //   e.preventDefault();
+      addWatched.textContent = 'Added from watched';
+      addWatched.disabled = true;
+      textWatchDelay();
+      addWatched.classList.add('active');
+    }
+  };
+  const onQueueModalBtn = e => {
+    console.log('hello!');
+    e.preventDefault();
 
-  //   if (addQueue.classList.contains('active')) {
-  //     console.log(data.id);
-  //     let index = queueArr.indexOf(data.id);
-  //     queueArr.splice(index, 1);
-  //     const stringedQueueArr = JSON.stringify(queueArr);
-  //     localStorage.setItem(QUEUE_KEY, stringedQueueArr);
-  //     console.log(stringedQueueArr);
-  //     Notiflix.Notify.failure('Removed from queue');
-  //     addQueue.classList.remove('active');
-  //     addQueue.textContent = 'Removed from queue';
-  //     addQueue.disabled = true;
-  //     textQueueDelayPre();
-  //   } else {
-  //     queueArr = [];
-  //     queueArr.push(...getLocalSt(QUEUE_KEY));
-  //     queueArr.push(queueObj);
-  //     stringedQueueArr = JSON.stringify(queueArr);
-  //     localStorage.setItem('queue', stringedQueueArr);
-  //     console.log(stringedQueueArr);
-  //     addQueue.textContent = 'Added from queue';
-  //     addQueue.disabled = true;
-  //     textQueueDelay();
-  //     addQueue.classList.add('active');
-  //   }
-  // };
-  // addWatched.addEventListener('click', onWatchedModalBtn);
-  // addQueue.addEventListener('click', onQueueModalBtn);
-  // //------------------------------------WATCHED-QUEUE---------------------------
+    if (addQueue.classList.contains('active')) {
+      console.log(data.id);
+      let index = queueArr.indexOf(data.id);
+      queueArr.splice(index, 1);
+      const stringedQueueArr = JSON.stringify(queueArr);
+      localStorage.setItem(QUEUE_KEY, stringedQueueArr);
+      console.log(stringedQueueArr);
+      Notiflix.Notify.failure('Removed from queue');
+      addQueue.classList.remove('active');
+      addQueue.textContent = 'Removed from queue';
+      addQueue.disabled = true;
+      textQueueDelayPre();
+    } else {
+      queueArr = [];
+      queueArr.push(...getLocalSt(QUEUE_KEY));
+      queueArr.push(queueObj);
+      stringedQueueArr = JSON.stringify(queueArr);
+      localStorage.setItem('queue', stringedQueueArr);
+      console.log(stringedQueueArr);
+      addQueue.textContent = 'Added from queue';
+      addQueue.disabled = true;
+      textQueueDelay();
+      addQueue.classList.add('active');
+    }
+  };
+  addWatched.addEventListener('click', onWatchedModalBtn);
+  addQueue.addEventListener('click', onQueueModalBtn);
+  //------------------------------------WATCHED-QUEUE---------------------------
   window.addEventListener('keydown', onEscCloseModal);
   refs.modal.addEventListener('click', onOutsideClickCloseModal);
 };
--- src/library.js
+++ src/library.js
@@ -1,41 +1,30 @@
 import '/src/sass/index.scss';
 import Notiflix from 'notiflix';
-import { filmTemplate } from './js/film-templete';
-import { refs, URL, KEY } from './index';
 
+
+var debounce = require('lodash.debounce');
 import { setLocalSt, getLocalSt, remLocalSt } from './js/localStorage';
+
+const DEBOUNCE_DELAY = 300;
+const KEY = '27a3692489226a6f77b57cb0bdb9ce9a';
+const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
+
+const refs = {
+  input: document.querySelector('.search-input'),
+  list: document.querySelector('.film-list'),
+  watched: document.querySelector('.watched-btn'),
+  queue: document.querySelector('.queue-btn'),
+  libraryList: document.querySelector('.films-list'),
+};
 
 const WATCHED_KEY = 'watched';
 const QUEUE_KEY = 'queue';
-
-const renderLibraryFilms = (arr, key) => {
-  const markup = arr.map(obj => filmTemplate(obj)).join('');
-
-  refs.list.insertAdjacentHTML('beforeend', markup);
+const onWatched = () => {
+  const watchFilm = getLocalSt(WATCHED_KEY);
+  render(watchFilm);
 };
 
-if (getLocalSt(WATCHED_KEY)) {
-  refs.list.innerHTML = '';
-  const watchFilm = [...getLocalSt(WATCHED_KEY)];
-  renderLibraryFilms(watchFilm, WATCHED_KEY);
-}
-// if (getLocalSt(QUEUE_KEY)) {
-//   refs.list.innerHTML = '';
-//   const queueFilm = [...getLocalSt(QUEUE_KEY)];
-//   renderLibraryFilms(queueFilm, QUEUE_KEY);
-// }
+const onQueue = () => {};
 
-// const onWatched = () => {
-//   refs.list.innerHTML = '';
-//   const watchFilm = [...getLocalSt(WATCHED_KEY)];
-//   renderLibraryFilms(watchFilm, WATCHED_KEY);
-// };
-
-// const onQueue = () => {
-//   refs.list.innerHTML = '';
-//   const queueFilm = [...getLocalSt(QUEUE_KEY)];
-//   renderLibraryFilms(queueFilm, QUEUE_KEY);
-// };
-
-// refs.watched.addEventListener('click', onWatched);
-// refs.queue.addEventListener('click', onQueue);
+refs.watched.addEventListener('click', onWatched);
+refs.queue.addEventListener('click', onQueue);
