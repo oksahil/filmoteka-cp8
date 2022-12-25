2bab8206fb7a415971545146172b334e8ed0a73e
Hakkar <zugrutus@gmail.com>
2022-12-25 18:29:21 +0000

    library again

--- src/library.js
+++ src/library.js
@@ -1,7 +1,6 @@
 import '/src/sass/index.scss';
 import Notiflix from 'notiflix';
-import { render } from '/src/index';
-import { refs } from '/src/index';
+import { filmTemplate } from './js/film-template-lib';
 
 var debounce = require('lodash.debounce');
 import { setLocalSt, getLocalSt, remLocalSt } from './js/localStorage';
@@ -10,15 +9,49 @@ const DEBOUNCE_DELAY = 300;
 const KEY = '27a3692489226a6f77b57cb0bdb9ce9a';
 const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
+const refs = {
+  input: document.querySelector('.search-input'),
+  list: document.querySelector('.film-list'),
+  watched: document.querySelector('.watched-btn'),
+  queue: document.querySelector('.queue-btn'),
+  libraryList: document.querySelector('.films-list-library'),
+};
+
 const WATCHED_KEY = 'watched';
 const QUEUE_KEY = 'queue';
 
-const onWatched = () => {
-  const watchFilm = getLocalSt(WATCHED_KEY);
-  render(watchFilm);
+const renderLibraryFilms = (arr, key) => {
+  const markup = arr.map(obj => filmTemplate(obj)).join('');
+
+  refs.list.insertAdjacentHTML('beforeend', markup);
 };
 
-const onQueue = () => {};
+if (!getLocalSt(WATCHED_KEY)) {
+  console.log('This is immaculately clean gallery ;)');
+}
+if (getLocalSt(WATCHED_KEY)) {
+  refs.list.innerHTML = '';
+  const watchFilm = [...getLocalSt(WATCHED_KEY)];
+  renderLibraryFilms(watchFilm, WATCHED_KEY);
+}
+if (getLocalSt(QUEUE_KEY)) {
+  refs.list.innerHTML = '';
+  const queueFilm = [...getLocalSt(QUEUE_KEY)];
+  renderLibraryFilms(queueFilm, QUEUE_KEY);
+}
+
+const onWatched = () => {
+  refs.list.innerHTML = '';
+  const watchFilm = [...getLocalSt(WATCHED_KEY)];
+  renderLibraryFilms(watchFilm, WATCHED_KEY);
+  Notiflix.Notify.success('added to watched');
+};
+
+const onQueue = () => {
+  refs.list.innerHTML = '';
+  const queueFilm = [...getLocalSt(QUEUE_KEY)];
+  renderLibraryFilms(queueFilm, QUEUE_KEY);
+};
 
 refs.watched.addEventListener('click', onWatched);
 refs.queue.addEventListener('click', onQueue);
