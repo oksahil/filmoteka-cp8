110ca0f0b8473962d1b62c5f12cf4b9d582bd27e
Hakkar <zugrutus@gmail.com>
2022-12-25 15:04:20 +0000

    library without modal

--- src/library.js
+++ src/library.js
@@ -1,78 +1,82 @@
 import '/src/sass/index.scss';
 import Notiflix from 'notiflix';
-import { filmTemplate, render, items, strGenres } from '.';
-import imgTemplate from '/src/images/BOX.jpg';
+import { filmTemplate } from './js/film-templete-lib';
+// import imgTemplate from '/src/images/BOX.jpg';
 
 
 var debounce = require('lodash.debounce');
 import { setLocalSt, getLocalSt, remLocalSt } from './js/localStorage';
-import { cardTemplate } from './js/card-templete';
 import axios from 'axios';
+import { fetchGenres } from './js/fetch-genres';
 
 const DEBOUNCE_DELAY = 300;
 const KEY = '27a3692489226a6f77b57cb0bdb9ce9a';
 const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
 
 
-const ID_URL = `${BASE_URL}/movie/`;
+const ID_URL = `https://api.themoviedb.org/3/search/movie/`;
 
 const refs = {
   input: document.querySelector('.search-input'),
   list: document.querySelector('.film-list'),
   watched: document.querySelector('.watched-btn'),
   queue: document.querySelector('.queue-btn'),
-  libList: document.querySelector('.film-list-library'),
+  filmList: document.querySelector('.film-list'),
 };
 
 const WATCHED_KEY = 'watched';
 const QUEUE_KEY = 'queue';
-const renderLibrary = (data) => {
-  const markup = libraryTpl(data);
 
-  return refs.libList.insertAdjacentHTML('beforeend', markup);
-}
+// const createMovieById = async (id) => {
+//   try {
+//     const { data } = await axios.get(`${ID_URL}${id}?api_key=${KEY}`);
 
-const createMovieById = async (id) => {
-  try {
-    const { data } = await axios.get(`${ID_URL}${id}?api_key=${KEY}`);
+//     const dataObj = {
+//       ...data,
+//       year: createYear(data),
+//       genre: onGenre(data),
+//     };
+//     return dataObj;
+//   } catch {
+//     console.log('Error', error);
+//   }
+// }
 
-    const dataObj = {
-      ...data,
-      year: createYear(data),
-      genre: onGenre(data),
-    };
-    return dataObj;
-  } catch {
-    console.log('Error', error);
-  }
-}
-
-const createYear = (obj) => {
-  return obj.release_date ? obj.release_date.split('-')[0] : '';
-}
-const onGenre = (arr) => {
-  return arr.genres.map(genre => genre.name).slice(0, 3).flat();
-}
+// const createYear = (obj) => {
+//   return obj.release_date ? obj.release_date.split('-')[0] : '';
+// }
+// const onGenre = (arr) => {
+//   return arr.genres.map(genre => genre.name).slice(0, 3).flat();
+// }
 
 const onWatched = () => {
-  const watchFilmId = [...getLocalSt(WATCHED_KEY)]
 
-  for (const id of watchFilmId) {
-    createMovieById(id).then(data => {
-      renderLibrary(data);
-    })
-  }
+  const watchFilm = [...getLocalSt(WATCHED_KEY)]
 
 
-  console.log(watchFilmId);
+
+  const markup = watchFilm.map(obj => filmTemplate(obj)).join('');
+
+  refs.filmList.insertAdjacentHTML('beforeend', markup);
+
+
+  console.log(markup);
 
   
 };
 
 const onQueue = () => {
-  const queueFilmId = [...getLocalSt(WATCHED_KEY)];
+  const queueFilm = [...getLocalSt(QUEUE_KEY)];
   
-  console.log(queueFilmId);
+  const markup = queueFilm.map(obj => {
+    const listg = obj.genres;
+    filmTemplate(obj)
+
+  });
+
+  refs.filmList.insertAdjacentHTML('beforeend', markup);
+
+  console.log(markup);
 };
 
 refs.watched.addEventListener('click', onWatched);
