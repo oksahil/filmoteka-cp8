cb88ba8fb0dbcb3114d927e4c7d16b250c844f16
oksahil <108234649+oksahil@users.noreply.github.com>
2022-12-25 14:37:24 +0100

    Revert "Library ready, not modal"

--- src/library.js
+++ src/library.js
@@ -1,7 +1,5 @@
 import '/src/sass/index.scss';
 import Notiflix from 'notiflix';
-import { filmTemplate } from './js/film-templete-lib';
-// import imgTemplate from '/src/images/BOX.jpg';
 
 
 var debounce = require('lodash.debounce');
@@ -16,42 +14,18 @@ const refs = {
   list: document.querySelector('.film-list'),
   watched: document.querySelector('.watched-btn'),
   queue: document.querySelector('.queue-btn'),
-  filmList: document.querySelector('.film-list'),
+  libraryList: document.querySelector('.films-list-library'),
 };
 
 const WATCHED_KEY = 'watched';
 const QUEUE_KEY = 'queue';
 
-
 const onWatched = () => {
-
-  const watchFilm = [...getLocalSt(WATCHED_KEY)]
-
-
-
-  const markup = watchFilm.map(obj => filmTemplate(obj)).join('');
-
-  refs.filmList.insertAdjacentHTML('beforeend', markup);
-
-
-  console.log(markup);
-
-  
+  const watchFilm = getLocalSt(WATCHED_KEY);
+  render(watchFilm);
 };
 
-const onQueue = () => {
-  const queueFilm = [...getLocalSt(QUEUE_KEY)];
-  
-  const markup = queueFilm.map(obj => {
-    const listg = obj.genres;
-    filmTemplate(obj)
-
-  });
-
-  refs.filmList.insertAdjacentHTML('beforeend', markup);
-
-  console.log(markup);
-};
+const onQueue = () => {};
 
 refs.watched.addEventListener('click', onWatched);
 refs.queue.addEventListener('click', onQueue);
