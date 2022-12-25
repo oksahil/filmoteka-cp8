760205c18b959831eef04d4cb3eb2d52af7f6c83
oksahil <108234649+oksahil@users.noreply.github.com>
2022-12-25 17:02:46 +0100

    Revert "library-func-half"

--- src/library.js
+++ src/library.js
@@ -1,6 +1,5 @@
 import '/src/sass/index.scss';
 import Notiflix from 'notiflix';
-import { filmTemplate } from './js/film-template-lib';
 
 
 var debounce = require('lodash.debounce');
@@ -21,34 +20,12 @@ const refs = {
 const WATCHED_KEY = 'watched';
 const QUEUE_KEY = 'queue';
 
-const renderLibraryFilms = (arr, key) => {
-  const markup = arr.map(obj => filmTemplate(obj)).join('');
-
-  refs.list.insertAdjacentHTML('beforeend', markup);
-};
-
-if (getLocalSt(WATCHED_KEY)) {
-  refs.list.innerHTML = '';
-  const watchFilm = [...getLocalSt(WATCHED_KEY)];
-  renderLibraryFilms(watchFilm, WATCHED_KEY);
-}
-if (getLocalSt(QUEUE_KEY)) {
-  refs.list.innerHTML = '';
-  const queueFilm = [...getLocalSt(QUEUE_KEY)];
-  renderLibraryFilms(queueFilm, QUEUE_KEY);
-}
-
 const onWatched = () => {
-  refs.list.innerHTML = '';
-  const watchFilm = [...getLocalSt(WATCHED_KEY)];
-  renderLibraryFilms(watchFilm, WATCHED_KEY);
+  const watchFilm = getLocalSt(WATCHED_KEY);
+  render(watchFilm);
 };
 
-const onQueue = () => {
-  refs.list.innerHTML = '';
-  const queueFilm = [...getLocalSt(QUEUE_KEY)];
-  renderLibraryFilms(queueFilm, QUEUE_KEY);
-};
+const onQueue = () => {};
 
 refs.watched.addEventListener('click', onWatched);
 refs.queue.addEventListener('click', onQueue);
