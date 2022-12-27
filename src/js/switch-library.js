9ac7b835dc04a5c0d513d436b7daca44c93c1da4
Na-diia <108739131+Na-diia@users.noreply.github.com>
2022-12-27 13:08:59 +0200

    Update switch-library.js

--- src/js/switch-library.js
+++ src/js/switch-library.js
@@ -11,16 +11,16 @@ const savedColor = localStorage.getItem("theme");
 
 function onChangeColor (event) {
  
- if(bodyRefs.classList.contains('light-theme') && blackFooter.classList.contains("light-theme")) {
-   bodyRefs.classList.remove('light-theme');
-   blackFooter.classList.remove('light-theme');
-   bodyRefs.classList.add('dark-theme');
-   blackFooter.classList.add('dark-theme');
+ if(bodyRefs.classList.contains('light-color-theme') && blackFooter.classList.contains("light-color-theme")) {
+   bodyRefs.classList.remove('light-color-theme');
+   blackFooter.classList.remove('light-color-theme');
+   bodyRefs.classList.add('dark-color-theme');
+   blackFooter.classList.add('dark-color-theme');
  } else {
-  bodyRefs.classList.remove('dark-theme');
-  blackFooter.classList.remove('dark-theme');
-  bodyRefs.classList.add('light-theme');
-  blackFooter.classList.add('light-theme');
+  bodyRefs.classList.remove('dark-color-theme');
+  blackFooter.classList.remove('dark-color-theme');
+  bodyRefs.classList.add('light-color-theme');
+  blackFooter.classList.add('light-color-theme');
   };
    
   localStorage.setItem('theme', bodyRefs.classList);
@@ -32,11 +32,11 @@ function checkingColor() {
    if(savedColor) {
    bodyRefs.classList = savedColor;
    blackFooter.classList = savedColor;
-   } else if(savedColor = 'dark-theme') {
-   blackFooter.classList.add('dark-theme');
-   bodyRefs.classList.add('dark-theme');
-  } else if ( savedColor === "light-theme") {
-    blackFooter.classList.add('light-theme');
-   bodyRefs.classList.add('light-theme');
+   } else if(savedColor = 'dark-color-theme') {
+   blackFooter.classList.add('dark-color-theme');
+   bodyRefs.classList.add('dark-color-theme');
+  } else if ( savedColor === "light-color-theme") {
+    blackFooter.classList.add('light-color-theme');
+   bodyRefs.classList.add('light-color-theme');
   };
 };
